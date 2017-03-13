2e815c2f997ad8c4b57b03a3c473dd4b630d552b
rahmanAlHapit <ral@student.ciputra.ac.id>
2017-03-13 12:03:08 +0700

    first commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List </title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2">
+				<h1>Todo List</h1>
+				<ul>
+					<li>Taki</li>
+					<li>Mitsuha</li>
+					<li>Yotsuha</li>
+				</ul>
+				<form>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="form-group">
+								<input type="text" class="form-control" name="todo_items" placeholder="Item baru....">		
+							</div>
+						</div>
+						<div class="col-lg-1">
+							<button type="submit" class="btn btn-success">Add</button>	
+						</div>
+					</div>
+				</form>
+
+				<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
